8ac3fa3d13078e097e16d87f8a63976d49728191
tarosun <sunxiaomingyes@gmail.com>
2021-06-16 20:53:07 +0800

    feat: 链接mongodb

---
+++ server/app/utils/mongo.js
@@ -0,0 +1,12 @@
+
+'use strict'
+
+const Mongodb = require('./mongodb')
+
+const {mongodbUrl, mongodbDbName} = require('../../config/db')
+
+function mongo() {
+    return new Mongodb(mongodbUrl, mongodbDbName)
+}
+
+module.exports = mongo
